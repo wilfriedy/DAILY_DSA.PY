40966cae1eb94def8561aef3fff4e08317c17efa
wilfriedy <wilfriedakpe3@gmail.com>
2023-11-21 19:46:51 +0100

    removing target

--- Day 2/remove_element.py
+++ Day 2/remove_element.py
@@ -1,8 +1,21 @@
-from typing import  *
+from typing import *
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         if not nums :
             return 0
         pointer = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[pointer] = nums[i]
+                pointer += 1
+
+        return pointer
+
+
+test_nums = [0,1,2,2,3,0,4,2]
+test_val = 2
+
+print(Solution().removeElement(test_nums, test_val))
 
