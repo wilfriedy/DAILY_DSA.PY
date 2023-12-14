a827740d1a655ad4f1e3401c8cbdf33c2fb7cb4b
wilfriedy <wilfriedakpe3@gmail.com>
2023-12-14 21:57:44 +0100

    revese string with stack

--- stacks-related-questions/beginner/stacks_1_01.py
+++ stacks-related-questions/beginner/stacks_1_01.py
@@ -6,12 +6,46 @@ class Stack:
     def __init__(self):
         self.items = []
 
-    def is_empty(self):
+    def is_empty(self): #check if stack is  
         return True if not self.items else False
 
     def push(self, item):
         self.items.append(item)
 
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
 
-# print(Stack().push(4))
-# print(Stack().is_empty())
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+def reverse_string (s):
+    stack = Stack()
+    if len(s) < 2:
+        return s
+
+    for char in s:
+        stack.push(char)
+
+    revStr = ''
+
+    while not stack.is_empty():
+        revStr += stack.pop()
+
+    return revStr
+
+
+input_str = "Hello, world!"
+reversed_str = reverse_string(input_str)
+print("Reversed String:", reversed_str)
+
+# newStack = Stack()
+# newStack.push(3)
+# newStack.pop()
+# newStack.pop()
+# print(newStack.items)
+# print(newStack.is_empty())
+
